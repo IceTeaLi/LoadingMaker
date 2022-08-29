e6b07df479e18da216d938782452fd2b1f20c096
Leon <IceTea_Li@outlook.com>
2022-08-29 17:51:30 +0800

    change CMakeLists

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: F:/Project/CrossPlatform/LoadingMaker
+# Build directory: F:/Project/CrossPlatform/LoadingMaker/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
